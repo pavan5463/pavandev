91bd425cabe5e87d0bcd7c6f16c911fc54d9226b
pavan5463 <60393330+pavan5463@users.noreply.github.com>
2021-05-07 01:56:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,180 +1,73 @@
-import streamlit as st
 import math
 import numpy as np
-import streamlit.components.v1 as components
+import datetime
 from flask import Flask, request, jsonify, render_template
+import pickle
 from model import Model
 
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-#def home():
-    #return render_template('login.html')
+@app.route('/recheck',methods=['POST','GET'])
+def recheck():
+    return render_template('index.html')
+	
+@app.route('/loginacc',methods=['POST','GET'])
+def loginacc():
+    return render_template('login.html')
 
-def app_run():
-    """
-    Returns
-    -------
-    Run App
-    
-    """
-    
-    #load Module class
-    # Create a text element and let the reader know the data is loading.                 
-    with st.spinner(text='In progress'):
-    #data_load_state = st.text('Loading data & model...')
-    # Notify the reader that the data was successfully loaded.
-        __model = Model()
-        st.success('Model Ready')
-    
-    #Dataset & zones
-    df = __model.dataset_preprocessed
-    
-    #Parking Space Price
-    parking_price_df =(
-        df.loc[df['parkingSpacePrice']>1000].
-            groupby('district').mean()['parkingSpacePrice'])
-    
-    #Title & Header Image
-    st.title('Evaluate your home in Telangana')
-    
-    st.subheader("Discover the market value of your home convenient and easy with one click")
-    
-    st.image('./data/Header Varese.jpg',use_column_width=True)
-    
-    st.subheader ("We use a Machine Learning algorithm on %s properties"
-         % len(df))
-    
-    #Parameters
-    st.subheader("Set the property parameters")
-    
-    #City
-    city = st.selectbox('City',['Warangal','Suryapet'],index = 0)
-    
-    selector_city = 'wG - '
-    
-    if city == 'warangal':
-        selector_city = 'WG - '
-        
-    else:
-        selector_city = 'SU - '
-    
-    #Zone
-    zonesList = (
-        df.loc[df['district'].str.startswith (selector_city)]['district'].unique().tolist()
-        )
-    
-    #Replace prefix
-    zonesList = [i.replace(selector_city,'') for i in zonesList]
-    
-    district = st.selectbox('Zone', zonesList, index = 0)
-    
-    #Property Type List
-    propertyTypelist = __model.propertyTypeList
+@app.route('/filedata',methods=['POST','GET'])
+def filedata():
+    return render_template('files.html')
 
-    propertyType = st.selectbox('Kind', propertyTypelist, index = 0)
-    
-    #Conversiont to model variables
-    propertyType = __model.propertyTypeConverter(propertyType)
-    
-    #Rest of parameters  
-    size = st.number_input('Square meters',
-              min_value=10, 
-              max_value=5000,
-              value = 100
-              )
-    
-    rooms = st.slider('Locals',
-            min_value = 1,
-            max_value =  10,
-            value = 3)
-        
-    #Conversiont to model variables
-    #roomsCat = __model.roomsCategory(rooms)
-    if rooms >= 4:
-        roomsCat = 4
-    else:
-        roomsCat = rooms
+@app.route('/cvr.ac.in',methods=['POST','GET'])
+def cvrweb():
+    return redirect(url_for('http://cvr.ac.in/home4/'))
+	
 
-    #Bathrooms
-    bathrooms = st.slider('Bathrooms',
-            min_value = 1,
-            max_value = 10,
-            value = 2
-                          )
-    #Conversiont to model variables
-    if bathrooms >= 2:
-            bathroomsCat = 2
-    else:
-        bathroomsCat = bathrooms
-
-    #Status italiano
-    status_it = __model.statusList
-    
-    status = st.radio('State',status_it, index = 1)
-    
-    #Conversiont to model variables
-    statusOutput = 'good'
-
-    if status == "To be restructured":
-        
-        statusOutput = 'renew'
-        
-    elif status == "Good":
-        
-        statusOutput = 'good'
-        
-    elif status == "New Construction ":
-    
-        statusOutput = 'newdevelopment'
-    #Extra Feautures
-    #parkingBox = st.checkbox('Posto Auto - Box', value = 0)
-        
-    #garden = st.checkbox('Giardino- Terrazzo', value = 0)
-    
-    #swimming_pool = st.checkbox('Piscina', value = 0)
-    
-    #Parking Space District Selected
-    
-    try:
-    
-        parking_space = int(
-            parking_price_df.loc[parking_price_df.index==
-                                 
-                                 (selector_city+district)].values[0])
-        
-    except:
-        
-        parking_space = 0
-            
-    #Button to value    
-    button = st.button('Predit Cost')
-        
-    if button:
-        
-        value_model = __model.predict( 
+	
+@app.route('/predict',methods=['POST','GET'])
+def predicts():  
+  #df = __model.dataset_preprocessed
+  
+  
+  
+  
+  
+  
+  
+  selector_city=request.form['city']
+  #Zone=request.form['zone']
+  propertyType=request.form['type']
+  size=request.form['size']
+  rooms=request.form['room']
+  bathrooms=request.form['bathroom']
+  status=request.form['status']
+  
+  #parking_price_df =(df.loc[df['parkingSpacePrice']>1000].groupby('district').mean()['parkingSpacePrice'])
+  
+  value_model =model.predict( 
             size,
             propertyType,
-            (selector_city+district),
+            (selector_city),
             statusOutput,
             roomsCat,
             bathroomsCat,
-            ) 
-    
-        value = int(math.ceil((value_model ) / 5000.0) * 5000.0)
-        
-        st.write("Market value")
-        st.write("₹{:,.0f}".format(value))
-        
-        if parking_space > 0:
-            
-            st.write("Average price  %s - %s " % (city,district))
-            st.write("₹{:,.0f}".format(parking_space))
-        
-        
-        
-    
+            )
+	
+  #parking_space = int(parking_price_df.loc[parking_price_df.index==(selector_city+district)].values[0])
+  return render_template('conform.html',value='{} %'.format(value))
+  
+
+
+
+  
 if __name__ == "__main__":
-    #app.run()
-    app_run()
-    
-    
+     #app.run(debug=True)
+     app.run(host="0.0.0.0",port=6442) 
+  
