22b9a7a838baa3b738350ce69d656504594a9475
pavan5463 <60393330+pavan5463@users.noreply.github.com>
2021-05-06 23:20:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,91 @@
 import streamlit as st
 import math
 import numpy as np
+import streamlit.components.v1 as components
 from flask import Flask, request, jsonify, render_template
 from model import Model
 
 
 
-def home():
-    return render_template('login.html')
+#def home():
+    #return render_template('login.html')
 
-def realapp():
+def app_run():
     """
     Returns
     -------
     Run App
     
     """
+    components.html("<head>
+  <meta charset="UTF-8">
+  <title>Login to RealEstate</title>
+
+	<script src="https://www.gstatic.com/firebasejs/5.0.4/firebase.js"></script>
+	<!--
+	  var config = {
+	    apiKey: "AIzaSyA1rRG77hpoYS3i6moo900KD8GR1wQ5Kh8",
+	    authDomain: "okcredit-7535e.firebaseapp.com",
+	    databaseURL: "https://okcredit-7535e.firebaseio.com",
+	    projectId: "okcredit-7535e",
+	    storageBucket: "okcredit-7535e.appspot.com",
+	    messagingSenderId: "870984863712"
+	  };
+	  firebase.initializeApp(config);
+	--> <script> var config = {
+apiKey: "AIzaSyCleAdT5AyqQR8LDibRw6874LxogcpTUks",
+  authDomain: "realestate-34d31.firebaseapp.com",
+  projectId: "realestate-34d31",
+  storageBucket: "realestate-34d31.appspot.com",
+  messagingSenderId: "552651165843",
+  appId: "1:552651165843:web:86367251ac1547fdd77d66",
+  measurementId: "G-98EG4HWFGX"
+};
+ firebase.initializeApp(config);
+</script>
+
+  <script src="https://cdn.firebase.com/libs/firebaseui/2.3.0/firebaseui.js"></script>
+  <link type="text/css" rel="stylesheet" href="https://cdn.firebase.com/libs/firebaseui/2.3.0/firebaseui.css" />
+  <!--<link href="style.css" rel="stylesheet" type="text/css" media="screen" />-->
+<link href="/static/style.css" rel="stylesheet" type="text/css" media="screen" />
+
+</head>
+<body style = "background-color:#6f70a6">
+    <div id="container">
+	    <h1><u>Login To RealEstate</u></h1>
+      <div id="loading">Loading...</div>
+      <div id="loaded" class="hidden">
+        <div id="main">
+          <div id="user-signed-in" class="hidden">
+            <div id="user-info">
+              <div id="phone">
+
+</div>
+              <div class="clearfix"></div>
+            </div>
+            <p>
+              <button id="sign-out">Sign Out</button>
+
+   </a>         </p>
+	      <form action="/realapp" class="hidden">
+ <button type="submit" class="btn-large waves-effect waves-light orange">Proceed to Webapp</button>
+</form>
+          </div>
+          <div id="user-signed-out" class="hidden">
+            <div id="firebaseui-spa">
+              <h3>App:</h3>
+              <div id="firebaseui-container"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="/static/app.js"></script>
+</body>")
     #load Module class
     # Create a text element and let the reader know the data is loading.
-    
+                    
+def realapp():                    
     with st.spinner(text='In progress'):
     #data_load_state = st.text('Loading data & model...')
     # Notify the reader that the data was successfully loaded.
@@ -174,6 +241,6 @@ def realapp():
     
 if __name__ == "__main__":
     #app.run()
-    #app_run()
-    home()
+    app_run()
+    
     
